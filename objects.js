f675e11ba321e19ed25809e171cb397acd374b62
sebastian <sverdu6@gmail.com>
2020-03-10 22:29:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(playlist, calamaro, flaca) {
   playlist[calamaro] = flaca
   return playlist
 }
+
+function removeFromPlaylist(playlist, calamaro) {
+  Delete playlist[calamaro];
+  return playlist
+}
